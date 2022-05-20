27b3b57daba2b44ce0006f10e365c98131c892bc
humanman <bensayeg@gmail.com>
2022-05-20 18:59:49 -0400

    updating congif + gitignore

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,6 +1,7 @@
 const { description } = require('../../package')
 
 module.exports = {
+  base: '/',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
@@ -27,7 +28,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: 'https://github.com/humanman/lms-test-nandf',
     editLinks: false,
     docsDir: '',
     editLinkText: '',
